7ff442b2b22f8e8f2cdbeaf7e395ecadae70b2b7
erin-a-z <ezzeuu@gmail.com>
2021-12-21 21:59:15 -0500

    Dec4 update

--- Dec4/Dec4-1.py
+++ Dec4/Dec4-1.py
@@ -9,7 +9,8 @@ print(inps)
 grids = []
 for i in range(numOfLines):
     if lines[i] == "":
-        grid = [[int(x) for x in lines[i + 1].split(" ") if x],[int(x) for x in lines[i + 2].split(" ") if x], [int(x) for x in lines[i + 3].split(" ") if x], [int(x) for x in lines[i + 4].split(" ") if x], [int(x) for x in lines[i + 5].split(" ") if x]]
+        grid = [[int(x) for x in lines[i + 1].split(" ") if x], [int(x) for x in lines[i + 2].split(" ") if x], [int(x)
+                                                                                                                 for x in lines[i + 3].split(" ") if x], [int(x) for x in lines[i + 4].split(" ") if x], [int(x) for x in lines[i + 5].split(" ") if x]]
         grids.append(grid)
 
 print(grids)
@@ -20,17 +21,21 @@ total = 0
 finalInp = 0
 while win == False and count < len(grids):
     inp = inps[count]
-    updatedGrid = [[[True if num == inp else num for num in row ] for row in singleGrid] for singleGrid in updatedGrid]
+    updatedGrid = [[[True if num == inp else num for num in row]
+                    for row in singleGrid] for singleGrid in updatedGrid]
     for singleGrid in updatedGrid:
-        row_same = [index if all(map(lambda x: x==True, i)) else -1 for index, i in enumerate(singleGrid)]
-        column_same = [j if all(map(lambda x: x==True, [i[j] for i in singleGrid])) else -1 for j in range(len(singleGrid[0]))]
+        row_same = [index if all(map(lambda x: x == True, i))
+                    else -1 for index, i in enumerate(singleGrid)]
+        column_same = [j if all(map(lambda x: x == True, [
+                                i[j] for i in singleGrid])) else -1 for j in range(len(singleGrid[0]))]
         if [x for x in row_same if x != -1] != [] or [x for x in column_same if x != -1] != []:
-            notCalled = [num for row in singleGrid for num in row if num != True]
+            notCalled = [
+                num for row in singleGrid for num in row if num != True]
             total = sum(notCalled)
             finalInp = inp
             win = True
 
     count += 1
 # calculate score
-fc = total * finalInp #final score
+fc = total * finalInp  # final score
 print(fc)
--- Dec4/Dec4-2.py
+++ Dec4/Dec4-2.py
@@ -1,4 +1,4 @@
-with open('Dec4/Dec4-data.txt') as f:
+with open('/Users/user1/Documents/Development/Python/Hydrogen/AdventOfCode 2021/Dec4/Dec4-data.txt') as f:
     lines = f.read().splitlines()
 
 numOfLines = len(lines)
@@ -10,7 +10,8 @@ gridCount = 0
 grids = []
 for i in range(numOfLines):
     if lines[i] == "":
-        grid = [[int(x) for x in lines[i + 1].split(" ") if x],[int(x) for x in lines[i + 2].split(" ") if x], [int(x) for x in lines[i + 3].split(" ") if x], [int(x) for x in lines[i + 4].split(" ") if x], [int(x) for x in lines[i + 5].split(" ") if x]]
+        grid = [[int(x) for x in lines[i + 1].split(" ") if x], [int(x) for x in lines[i + 2].split(" ") if x], [int(x)
+                                                                                                                 for x in lines[i + 3].split(" ") if x], [int(x) for x in lines[i + 4].split(" ") if x], [int(x) for x in lines[i + 5].split(" ") if x]]
         gridCount += 1
         grids.append(grid)
 
@@ -23,14 +24,18 @@ total = 0
 finalInp = 0
 while last == False and count < gridCount:
     inp = inps[count]
-    updatedGrid = [[[True if num == inp else num for num in row ] for row in singleGrid] for singleGrid in updatedGrid]
+    updatedGrid = [[[True if num == inp else num for num in row]
+                    for row in singleGrid] for singleGrid in updatedGrid]
     for index, singleGrid in enumerate(updatedGrid):
-        row_same = [index if all(map(lambda x: x==True, i)) else -1 for index, i in enumerate(singleGrid)]
-        column_same = [j if all(map(lambda x: x==True, [i[j] for i in singleGrid])) else -1 for j in range(len(singleGrid[0]))]
+        row_same = [index if all(map(lambda x: x == True, i))
+                    else -1 for index, i in enumerate(singleGrid)]
+        column_same = [j if all(map(lambda x: x == True, [
+                                i[j] for i in singleGrid])) else -1 for j in range(len(singleGrid[0]))]
         if [x for x in row_same if x != -1] != [] or [x for x in column_same if x != -1] != []:
             win[index] = True
             if [w for w in win if w != True] == []:
-                notCalled = [num for row in singleGrid for num in row if num != True]
+                notCalled = [
+                    num for row in singleGrid for num in row if num != True]
                 finalInp = inp
                 total = sum(notCalled)
                 last = True
@@ -38,5 +43,5 @@ while last == False and count < gridCount:
     count += 1
 # calculate score
 
-fc = total * finalInp #final score
+fc = total * finalInp  # final score
 print(fc)
